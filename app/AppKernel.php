c0e8f04a48a39bb8595569739186a762e85d7eb7
mswebmaster <m.sifflet@ly610106.local>
2014-04-03 10:55:54 +0200

    Add AdminBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Unetwork\PublicBundle\UnetworkPublicBundle(),
+            new Unetwork\AdminBundle\UnetworkAdminBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
