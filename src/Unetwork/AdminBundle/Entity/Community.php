e528dd291ab208e3c803ddbfbb5ea4c8b38f1379
ikenga89 <x.guien@ly610108.local>
2014-04-03 14:02:29 +0200

    ENtity

---
+++ src/Unetwork/AdminBundle/Entity/Community.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Unetwork\AdminBundle\Entity;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="community")
+ */
+class Community
+{
+	/**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+	/**
+     * @ORM\Column(type="string", length=200)
+     */
+    protected $name;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Community
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
