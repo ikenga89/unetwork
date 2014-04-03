2f092d684e844066aaaf7e16d9e2105df9afa890
Yang-Eric <e.yang@ly610112.local>
2014-04-03 14:39:50 +0200

    Add commentaire entity

---
+++ src/Unetwork/AdminBundle/Entity/Commentaire.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace Unetwork\AdminBundle\Entity;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="commentaire")
+ */
+class Commentaire
+{
+	/**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+	/**
+     * @ORM\Column(type="string", length=100)
+     */
+    protected $name;
+
+
+	/**
+     * @ORM\Column(type="string", length=100)
+     */
+    protected $prenom;
+
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $date;
+
+
+	/**
+     * @ORM\Column(type="string", length=500)
+     */
+    protected $contenu;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Commentaire
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set prenom
+     *
+     * @param string $prenom
+     * @return Commentaire
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    /**
+     * Get prenom
+     *
+     * @return string 
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     * @return Commentaire
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime 
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set contenu
+     *
+     * @param string $contenu
+     * @return Commentaire
+     */
+    public function setContenu($contenu)
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+
+    /**
+     * Get contenu
+     *
+     * @return string 
+     */
+    public function getContenu()
+    {
+        return $this->contenu;
+    }
+}
