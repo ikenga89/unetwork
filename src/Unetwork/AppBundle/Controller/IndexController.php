11fdeeec7a95dc2fa472a5351043573cb240a373
Yang-Eric <yang_eric@hotmail.fr>
2014-05-20 19:55:14 +0200

    Requete DQL

--- src/Unetwork/AppBundle/Controller/IndexController.php
+++ src/Unetwork/AppBundle/Controller/IndexController.php
@@ -5,6 +5,7 @@ namespace Unetwork\AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 
 class IndexController extends Controller
 {
@@ -12,11 +13,26 @@ class IndexController extends Controller
      * @Route("/app/index", name="app_index")
      * @Template()
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         $actualities = $this->getDoctrine()
         ->getRepository('UnetworkAdminBundle:Actuality')
         ->findAll();
+
+        $defaultData = array();
+        $form = $this->createFormBuilder($defaultData)
+            ->add('recherche', 'text')
+            ->add('rechercher', 'submit')
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            // Les données sont un tableau avec les clés "name", "email", et "message"
+            $data = $form->getData();
+
+            return $this->redirect($this->generateUrl('app_recherche', array('text' => $data['recherche'])));
+        }
 
         /*
         $user = new user();
@@ -40,6 +56,31 @@ class IndexController extends Controller
             "actualities" => $actualities,
             "user" => $user,
         );
+
+    }
+
+    /**
+     * @Route("/app/recherche/{text}", name="app_recherche")
+     * @Template()
+     */
+    public function rechercheAction(Request $request){
+
+        // REQUETE
+        // On recupere l'objet depuis Doctrine 
+        $em = $this->getDoctrine()->getManager();
+
+        // Requete DQL 
+        $query = $em->createQuery(
+        'SELECT *
+         FROM UnetworkAppBundle:unetwork_users
+         WHERE nom = [recherche]' // Cherche le nom de l'user entrer par l'utilisateur
+        )->setParameter('nom');
+
+        $products = $query->getResult();
+        //
+
+
+        // AFFICHAGE
 
     }
 }
