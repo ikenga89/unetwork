82d4204006d8387ced88eb453dfc88e36db37a14
cedricono <cedricono@hotmail.fr>
2014-05-20 19:54:40 +0200

    community

--- web/Admin/js/community_request.js
+++ web/Admin/js/community_request.js
@@ -30,9 +30,14 @@ var communities_updating = true; // pour empecher double clic abusif
 
 function edit_community(THIS,ID){
 	if (communities_updating) {
-		var community_id = "#community_" + ID;
-		var community_name = $( community_id ).html();
-		$( community_id ).html('<input class="input" maxlength="200" type="texte" value="' + community_name + '">');
+		var community_name_id = "#community_name_" + ID;
+		var community_name = $( community_name_id ).html();
+		$( community_name_id ).html('<input class="input_name" maxlength="200" type="texte" value="' + community_name + '">');
+
+		var community_alias_id = "#community_alias_" + ID;
+		var community_alias = $( community_alias_id ).html();
+		$( community_alias_id ).html('<input class="input_alias" maxlength="4" type="texte" value="' + community_alias + '">');
+
 		var onclick = "update_community(this," + ID + ")";
 		$( THIS ).attr( "onclick" , onclick );
 		$( THIS ).attr("src","/Admin/img/validate.png");
@@ -43,18 +48,26 @@ function update_community(THIS,ID){
 	if (communities_updating) {
 		communities_updating = false;
 		$( THIS ).attr("src","/Admin/img/validate_grey.png");
-		var community_id = "#community_" + ID + " " + "input";
-		var community_name = $.trim( $( community_id ).val() );
+
+		var community_name_id = "#community_name_" + ID + " " + "input";
+		var community_name = $.trim( $( community_name_id ).val() );
+
+		var community_alias_id = "#community_alias_" + ID + " " + "input";
+		var community_alias = $.trim( $( community_alias_id ).val() );
 
 		$.ajax({
 		  type: "POST",
 		  url: link_update_community,
-		  data: { post_community_id: ID, post_community_name: community_name, }
+		  data: { post_community_id: ID, post_community_name: community_name, post_community_alias: community_alias,}
 
 		}).done(function( reponse ) {
-			if( reponse == 1 ){
-				community_id = "#community_" + ID;
-				$( community_id ).html( community_name );
+			if( reponse == 1 ) {
+				community_name_id = "#community_name_" + ID;
+				$( community_name_id ).html( community_name );
+
+				community_alias_id = "#community_alias_" + ID;
+				$( community_alias_id ).html( community_alias );
+
 				$( THIS ).attr("src","/Admin/img/validate_green.png");
 				setTimeout( function(){ $( THIS ).attr("src","/Admin/img/edit.png"); }, 1000);
 				var onclick = "edit_community(this," + ID + ")";
