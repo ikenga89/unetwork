f8c980da54b65f81548a5fd82db452120b625ab8
ikenga89 <xavier.guien@gmail.com>
2014-06-19 23:15:44 +0200

    Update offres

--- src/Unetwork/AppBundle/Controller/OffreController.php
+++ src/Unetwork/AppBundle/Controller/OffreController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-class offreController extends Controller
+class OffreController extends Controller
 {
     /**
      * @Route("/app/offre", name ="app_offre" )
